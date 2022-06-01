10a79a68283e5f2a0f616ca368abc241d1f05b06
Johannes Courtial <johannes.courtial@glasgow.ac.uk>
2022-06-01 10:26:08 +0100

    RectangularPixelDiffraction now hopefully completed

--- src/optics/raytrace/surfaces/RectangularPixelDiffraction.java
+++ src/optics/raytrace/surfaces/RectangularPixelDiffraction.java
@@ -1,6 +1,7 @@
 package optics.raytrace.surfaces;
 
 import math.Vector3D;
+import optics.raytrace.utility.SingleSlitDiffraction;
 
 /**
  * @author johannes
@@ -10,14 +11,38 @@ public class RectangularPixelDiffraction extends Diffusion
 {
 	private static final long serialVersionUID = 6023321133278951887L;
 
-	public RectangularPixelDiffraction()
+	private double lambda;
+	private double pixelSideLengthU;
+	private double pixelSideLengthV;
+	private Vector3D uHat;
+	private Vector3D vHat;
+
+	public RectangularPixelDiffraction(
+			double lambda,
+			double pixelSideLengthU,
+			double pixelSideLengthV,
+			Vector3D uHat,
+			Vector3D vHat
+		)
 	{
 		super();
+		
+		this.lambda = lambda;
+		this.pixelSideLengthU = pixelSideLengthU;
+		this.pixelSideLengthV = pixelSideLengthV;
+		this.uHat = uHat;
+		this.vHat = vHat;
 	}
 	
 	public RectangularPixelDiffraction(RectangularPixelDiffraction original)
 	{
-		super();
+		this(
+				original.getLambda(),
+				original.getPixelSideLengthU(),
+				original.getPixelSideLengthV(),
+				original.getuHat(),
+				original.getvHat()
+			);
 	}
 	
 	@Override
@@ -26,9 +51,56 @@ public class RectangularPixelDiffraction extends Diffusion
 		return new RectangularPixelDiffraction(this);
 	}
 	
+	
+	//
+	// setters & getters
+	//
+	
+	public double getLambda() {
+		return lambda;
+	}
+
+	public void setLambda(double lambda) {
+		this.lambda = lambda;
+	}
+
+	public double getPixelSideLengthU() {
+		return pixelSideLengthU;
+	}
+
+	public void setPixelSideLengthU(double pixelSideLengthU) {
+		this.pixelSideLengthU = pixelSideLengthU;
+	}
+
+	public double getPixelSideLengthV() {
+		return pixelSideLengthV;
+	}
+
+	public void setPixelSideLengthV(double pixelSideLengthV) {
+		this.pixelSideLengthV = pixelSideLengthV;
+	}
+
+	public Vector3D getuHat() {
+		return uHat;
+	}
+
+	public void setuHat(Vector3D uHat) {
+		this.uHat = uHat;
+	}
+
+	public Vector3D getvHat() {
+		return vHat;
+	}
+
+	public void setvHat(Vector3D vHat) {
+		this.vHat = vHat;
+	}
+
+	
 	//
 	// Diffusion methods
 	//
+
 
 	/* (non-Javadoc)
 	 * @see optics.raytrace.surfaces.Diffusion#calculateDirectionChange(math.Vector3D, math.Vector3D)
@@ -39,8 +111,13 @@ public class RectangularPixelDiffraction extends Diffusion
 			Vector3D normalisedOutwardsSurfaceNormal
 		)
 	{
-		// TODO
-		return null;
+		return SingleSlitDiffraction.getTangentialDirectionComponentChange(
+				lambda,
+				pixelSideLengthU,	// pixelSideLengthU
+				pixelSideLengthV,	// pixelSideLengthV
+				uHat,	// uHat
+				vHat	// vHat
+			);
 	}
 	
 
